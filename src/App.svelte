fcb3ef6aa119ecc24849ff8b312636c50370304a
Vishan Patel <vishan.patel@ontariotechu.net>
2025-11-11 04:47:34 -0500

    added profit over time graph

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,336 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+    import { onMount } from 'svelte';
+    import InputField from './components/InputField.svelte';
+    import Plotly from 'plotly.js-dist-min';
+    import { Client, Functions } from 'appwrite';
+
+    let ProfitOverTimeChart;
+    let profitOverTimeLayout;
+
+    // inputs
+    let entryTime = "";
+    let spreadWidth = "";
+    let entryCredit = "";
+    let numberOfSpreads = "";
+    let stopPrice = "";
+    let limitPrice = "";
+    let stopLossMultiplier = "";
+
+    // sets graph height to match input container height
+    onMount(() => {
+      const input = document.getElementById('container-input');
+      const graph = document.getElementById('container-graph');
+      graph.style.height = `${input.offsetHeight}px`;
+    });
+
+    // initalize plotly chart
+    onMount(() => {
+      profitOverTimeLayout = {
+        title: {
+          text: 'Profit Over Time',
+          font: {
+            color: 'white',
+            size: 15
+          },
+          x: 0.5
+        },
+        margin: {
+          t: 25,
+          b: 50,
+          l: 60,
+          r: 25
+        },
+        autosize: true,
+        xaxis: {
+          title: {
+            text: 'Date',
+            font: {
+              size: 15,
+              color: 'white'
+            }
+          },
+          tickfont: {
+            color: '#C9C9CA',
+            size: 12
+          },
+          linecolor: '#C9C9CA',
+          type: 'date',
+          tickformat: '%b %d',
+          range: ['2024-02-12', '2024-02-16'],
+          dtick: 86400000,
+        },
+        yaxis: {
+          title: {
+            text: 'Profit',
+            font: {
+              size: 15,
+              color: 'white'
+            },
+            standoff: 5,
+          },
+          automargin: true,
+          tickfont: {
+            color: '#C9C9CA',
+            size: 12
+          },
+          linecolor: '#C9C9CA',
+          range: [0, 100],
+        },
+        showlegend: false,
+        paper_bgcolor: '#161616',
+        plot_bgcolor: '#161616',
+      };
+      
+      Plotly.newPlot(ProfitOverTimeChart, [{x: [], y: [], type: 'scatter'}], profitOverTimeLayout, { responsive: true });
+      Plotly.Plots.resize(ProfitOverTimeChart);
+    });
+
+    // plot graph
+    function plotProfitOverTime(dates, profitOverTime) {
+      // format to yyyy-mm-dd
+      for (let i=0; i<dates.length; i++) {
+        const day = dates[i].slice(6,8);
+        const month = dates[i].slice(4,6);
+        const year = dates[i].slice(0,4);
+        dates[i] = `${year}-${month}-${day}`;
+      }
+
+      // adjust y-axis range based on data
+      const max = (Math.max(...profitOverTime) / 10) * 10;
+      const min = (Math.min(...profitOverTime) / 10) * 10;
+      if (min >= 0) {
+        profitOverTimeLayout.yaxis.range = [0, max + 100];
+      } else if (max <= 0) {
+        profitOverTimeLayout.yaxis.range = [min - 100, 0];
+      } else {
+        profitOverTimeLayout.yaxis.range = [min - 100, max + 100];
+      }
+
+      // interpolate zero crossings
+      let newDates = [];
+      let newProfits = [];
+      let fakeReal = [];
+      for (let i=1; i<profitOverTime.length; i++) {
+        let prevDate = dates[i-1];
+        let prevPrice = profitOverTime[i-1];
+        let curDate = dates[i];
+        let curPrice = profitOverTime[i];
+
+        // add first point
+        if (i === 1) {
+          newDates.push(prevDate);
+          newProfits.push(prevPrice);
+          fakeReal.push('real');
+        }
+
+        // detect sign change
+        if (Math.sign(prevPrice) !== Math.sign(curPrice)) {
+          //add time halfway between prev date and cur date
+          let midDate = new Date((new Date(prevDate).getTime() + new Date(curDate).getTime()) / 2). toISOString().replace('.000Z', '');
+          newDates.push(midDate);
+          newProfits.push(0);
+          fakeReal.push('fake');
+        }
+
+        // add current point
+        newDates.push(curDate);
+        newProfits.push(curPrice);
+        fakeReal.push('real');
+      }
+
+      const positiveTrace = {
+        x: newDates,
+        y: newProfits.map(val => val >= 0 ? val : null),
+        type: 'scatter',
+        mode: 'lines',
+        line: { color: '#339D01', width: 3 },
+        fill: 'tozeroy',
+        fillcolor: 'rgba(51, 157, 1, 0.2)',
+        hoverinfo: 'skip'
+      };
+
+      const negativeTrace = {
+        x: newDates,
+        y: newProfits.map(val => val <= 0 ? val : null),
+        type: 'scatter',
+        mode: 'lines',
+        line: { color: '#DF2828', width: 3 },
+        fill: 'tozeroy',
+        fillcolor: 'rgba(223, 40, 40, 0.2)',
+        hoverinfo: 'skip'
+      };
+
+      const originalTrace = {
+        x: dates,
+        y: profitOverTime,
+        type: 'scatter',
+        mode: 'markers',
+        marker: { color: 'white', size: 5 },
+        hovertemplate: '<b>Date:</b> %{x}<br><b>Profit:</b> %{y}<extra></extra>'
+      };
+      
+      // update plotly chart
+      Plotly.newPlot(ProfitOverTimeChart, [positiveTrace, negativeTrace, originalTrace], profitOverTimeLayout, { responsive: true });
+      Plotly.Plots.resize(ProfitOverTimeChart);
+    }
+
+    // appwrite client setup
+    const client = new Client()
+    .setEndpoint('https://tor.cloud.appwrite.io/v1')
+    .setProject('690eb041002cafd8d0c7');
+    const functions = new Functions(client);
+
+    async function getResults() {
+      const parameters = {
+        entryTime,
+        spreadWidth,
+        entryCredit,
+        numberOfSpreads,
+        stopPrice,
+        limitPrice,
+        stopLossMultiplier
+      };
+      console.log("data to send:", parameters);
+
+      try {
+        // call appwrite function
+        const execution = await functions.createExecution(
+          '690eb25c0005266fa532',
+          JSON.stringify(parameters),
+          false // false = synchronous (wait for response), true = asynchronous (do not wait)
+        );
+
+        // parse response
+        try {
+          let functionResponse = JSON.parse(execution.responseBody).response;
+
+          const totalProfit = functionResponse.totalProfit;
+          const dates = functionResponse.dates;
+          const profitOverTime = functionResponse.profitOverTime;
+
+          plotProfitOverTime(dates, profitOverTime);
+        } catch (error) {
+            console.error('failed to parse response:', error);
+        }
+      } catch (error) {
+        console.error('error:', error);
+      }
+    }
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
+  <div class="top-nav-bar">
+    <h1 id="top-nav-bar-title">Backtester</h1>
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+  <div class="container-horizontal">
+    <!-- input parameters -->
+    <div class="container-child" id="container-input">
+        <h2>Configuration</h2>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+        <div class="container-horizontal">
+          <InputField Label="Entry Time" bind:Value={entryTime}/>
+          <InputField Label="Spread Width" bind:Value={spreadWidth}/>
+        </div>
+
+        <div class="container-horizontal">
+          <InputField Label="Entry Credit" bind:Value={entryCredit}/>
+          <InputField Label="Number of Spreads" bind:Value={numberOfSpreads}/>
+        </div>
+
+        <div class="container-horizontal">
+          <InputField Label="Stop Price" bind:Value={stopPrice}/>
+          <InputField Label="Limit Price" bind:Value={limitPrice}/>
+        </div>
+
+        <div class="container-horizontal">
+          <InputField Label="Stop Loss Multiplier" bind:Value={stopLossMultiplier}/>
+          <div id="BacktestButtonContainer">
+            <button id="BacktestButton" type="button" on:click={getResults}>Run Backtest</button>
+          </div>
+        </div>
+    </div>
+
+    <!-- graph -->
+    <div class="container-child" id="container-graph">
+      <div id="ProfitOverTimeChart" bind:this={ProfitOverTimeChart}></div>
+    </div>
+  </div>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  h1 {
+    font-weight: bold;
+    font-size: 2.5rem;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  h2 {
+    font-weight: bold;
+    font-size: 1.25rem;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+
+  .top-nav-bar {
+    background-color: rgb(0, 0, 0);
+    color: rgb(255, 255, 255);
+    font-weight: bold;
+    font-size: 2.5rem;
   }
-  .read-the-docs {
-    color: #888;
+
+  #top-nav-bar-title {
+    margin-left: 2rem;
   }
+
+  .container-horizontal {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: flex;
+    gap: 1rem;
+    margin: 1rem;
+  }
+
+  .container-child {
+    background-color: #161616;
+    padding: 1rem;
+  }
+
+  #container-input {
+    width: 50%;
+  }
+
+  #container-graph {
+    width: 50%;
+    overflow: hidden;
+  }
+  
+  #ProfitOverTimeChart {
+    width: 100%;
+    height: 100%;
+  }
+
+  #BacktestButtonContainer {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end; 
+  }
+
+  #BacktestButton {
+    width: 100%;
+    height: 1.75rem;
+    background-color: #0C40C2;
+    border: none;
+    text-align: center;
+    color: white;
+    cursor: pointer;
+  }
+
+  #BacktestButton:hover {
+    background-color: #0A30A0;
+  }
+
+  #BacktestButton:active {
+    background-color: #082080;
+  }
+
 </style>
---
+++ src/components/InputField.svelte
@@ -0,0 +1,82 @@
+<script>
+  export let Label = "";
+  export let Value = "";
+
+  switch (Label) {
+    case "Entry Time":
+      Value = "10:00";
+      break;
+    case "Spread Width":
+      Value = "30";
+      break;
+    case "Entry Credit":
+      Value = "1.5";
+        break;
+    case "Number of Spreads":
+        Value = "3";
+        break;  
+    case "Stop Price":
+        Value = "1.1";
+        break;
+    case "Limit Price":
+        Value = "1";
+        break;
+    case "Stop Loss Multiplier":
+        Value = "2";
+        break;
+    default:
+      Value = "";
+      break;
+  }
+</script>
+
+<div class="input-field-container">
+    <p class="input-field-label">{Label}</p>
+
+    {#if Label === "Entry Time"}
+        <input type="text" name="input-{Label}" required maxLength="5" bind:value={Value}>
+    {:else if Label === "Spread Width"}
+        <input type="text" name="input-{Label}" required maxlength="3" bind:value={Value}>
+    {:else if Label === "Entry Credit"}
+        <input type="text" name="input-{Label}" required maxlength="3" bind:value={Value}>
+    {:else if Label === "Number of Spreads"}
+        <input type="text" name="input-{Label}" required maxlength="1" bind:value={Value}>
+    {:else if Label === "Stop Price"}
+        <input type="text" name="input-{Label}" required maxlength="3" bind:value={Value}>
+    {:else if Label === "Limit Price"}
+        <input type="text" name="input-{Label}" required maxlength="3" bind:value={Value}>
+    {:else if Label === "Stop Loss Multiplier"}
+        <input type="text" name="input-{Label}" required maxlength="3" bind:value={Value}>
+    {/if}
+</div>
+
+<style>
+    .input-field-container {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        gap: 0.25rem;
+        width: 100%;
+    }
+
+    .input-field-label {
+        padding-left: 0.5rem;
+    }
+
+    input {
+        width: 100%;
+        height: 1.75rem;
+        background-color: #242424;
+        border: none;
+        text-align: right;
+        padding-right: 0.75rem;
+    }
+
+    input:hover {
+        outline: 2px solid #4e4e4e;
+    }
+
+    input:focus {
+        outline: 2px solid #888888;
+    }
+</style>
